5572c395048d24ac9c9732b878e9b1c6a88d44a4
lzmoj0906 <149756889+lzmoj0906@users.noreply.github.com>
2025-12-26 14:14:20 +0800

    Update print statement from 'Hello' to 'Goodbye'

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -338,7 +338,7 @@ export default {
           "suo.yt": "https://suo.yt/short",
         },
         customBackend: {
-          "自用专属后端": "https://subapi.mojia.xyz",// 为您的后端取名
+          "自用专属后端": "https://subapi.992844.xyz",// 为您的后端取名
           "CM负载均衡后端【vless reality+hy1+hy2】": "https://subapi.cmliussss.net",
           "CM应急备用后端【vless reality+hy1+hy2】": "https://subapi.fxxk.dedyn.io",
           "肥羊增强型后端【vless reality+hy1+hy2】": "https://url.v1.mk",
@@ -1312,3 +1312,4 @@ export default {
   }
 };
 </script>
+
